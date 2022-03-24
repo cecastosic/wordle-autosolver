933185517e437c67885f6b98216044106b26a925
Svetlana <cecastosic@gmail.com>
2022-03-24 12:09:04 +0100

    added dictionary module, list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dictionary',
+    loadChildren: () => import('./dictionary/dictionary.module').then(m => m.DictionaryModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
